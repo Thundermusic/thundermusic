08161291b44a0d531b68e09b942358d319057359
Duarte David <deltaduartedavid@gmail.com>
2018-10-16 15:35:29 +0200

    Replace default icons

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,13 @@ module.exports = {
         workboxOptions: {
             swSrc: './src/sw.js'
         },
-        themeColor: '#f67504'
+        themeColor: '#f67504',
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: null,
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        }
     }
 };3
